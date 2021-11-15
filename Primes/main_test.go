2240fe0dfe15dd4008e03986985bc59ae57b408d
Marc Lavergne <marc.lavergne@hotmail.com>
2021-11-14 21:06:19 -0600

    got initial results

--- Primes/main_test.go
+++ Primes/main_test.go
@@ -10,12 +10,12 @@ func BenchmarkPrimes(b *testing.B) {
 	functions := []struct {
 		name string
 		fun  (func(x uint64) []uint64)
-	}{{"Single Threaded", findPrimes}, {"Multi Threaded", findPrimesParallel}}
+	}{{"Single Threaded1e", findPrimes}, {"Multi Threaded1e", findPrimesParallel}}
 
 	for _, v := range [...]uint64{1, 2, 3, 4, 5, 6, 7, 8} {
 		maxVal := uint64(math.Pow(10, float64(v)))
 		for _, f := range functions {
-			b.Run(fmt.Sprintf("%s%d", f.name, maxVal), func(b *testing.B) {
+			b.Run(fmt.Sprintf("%s%dT", f.name, v), func(b *testing.B) {
 				for bench := 0; bench < b.N; bench++ {
 					f.fun(maxVal)
 				}
